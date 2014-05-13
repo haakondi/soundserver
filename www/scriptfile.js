c2ac5fe6bcdaff279d24457622d6fde4bf293d3e
haakondi <hakon.dissen@gmail.com>
2014-05-13 21:10:54 +0200

    fixed something

--- www/scriptfile.js
+++ www/scriptfile.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   var fixer = function( data ) {
       $( ".result" ).html( data );
   };
-  var songID = parseInt($("#song_id_input").val(), 10);
    $('#play_button').click(function() {
+      var songID = parseInt($("#song_id_input").val(), 10);
       $.post( "/index.html",'{command_container : {command : "play_new",song_id :' + songID + '}}', fixer);
    }); 
 });
