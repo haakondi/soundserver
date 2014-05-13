f0623793255e9fa754107ca3b727a3bbdd7d7ff7
Torstein <torsteinsandven@gmail.com>
2014-05-13 23:07:41 +0200

    Begynt på layouten

--- www/torstein/scriptfile.js
+++ www/torstein/scriptfile.js
@@ -3,6 +3,13 @@ $(document).ready(function() {
       $( ".result" ).html( data );
   };
    $('#play_button').click(function() {
-      $.post( "/index.html", fixer);
+      var songID = parseInt($("#song_id_input").val(), 10);
+      $.post( "/index.html",'{command_container : {command : "play_new",song_id :' + songID + '}}', fixer);
+   }); 
+   $('#pause_button').click(function() {
+      $.post( "/index.html",'{command_container : {command : "pause"}}', fixer);
+   }); 
+   $('#resume_button').click(function() {
+      $.post( "/index.html",'{command_container : {command : "resume"}}', fixer);
    }); 
 });
