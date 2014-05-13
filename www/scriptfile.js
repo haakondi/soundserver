a45e3222e58809f6fd05e81fa065b84dcf683b81
haakondi <hakon.dissen@gmail.com>
2014-05-13 21:57:15 +0200

    fixed some more

--- www/scriptfile.js
+++ www/scriptfile.js
@@ -6,4 +6,10 @@ $(document).ready(function() {
       var songID = parseInt($("#song_id_input").val(), 10);
       $.post( "/index.html",'{command_container : {command : "play_new",song_id :' + songID + '}}', fixer);
    }); 
+   $('#pause_button').click(function() {
+      $.post( "/index.html",'{command_container : {command : "pause"}}', fixer);
+   }); 
+   $('#resume_button').click(function() {
+      $.post( "/index.html",'{command_container : {command : "resume"}}', fixer);
+   }); 
 });
