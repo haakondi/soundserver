dc886848b04085783be28bae64780a61cab24a1d
Josteinas <josteinsolaas@gmail.com>
2014-05-14 17:54:10 +0200

    scrollable clickable table elements

--- www/jostein/script.js
+++ www/jostein/script.js
@@ -1,18 +1,23 @@
 
 var listSongs = function( data ) {
 	var songs = ($.parseJSON(data)).song_list;
+  console.log(songs);
 	for (var i = 0; i < songs.length; i++) {
 		var song = songs[i];
-		$('#songTable').append('<tr><td>'+song.track_name+'</td><td>'+song.artist+'</td><td>'+song.album+'</td></tr>');
+		$('#songTable').append('<tr class="songElement" id="'+i+'"><td>'+song.track_name+'</td><td>'+song.artist+'</td><td>'+song.album+'</td></tr>');
 	};
+  $('.songElement').click(function() {
+      console.log("clicked!");
+      var songID = $(this).attr('id');
+      playSong(songID);
+  });
+}
+
+function playSong(id) {
+  $.post( "/",'{command_container : {command : "play_new" , song_id : '+ id +'}}');
 }
 
 $(document).ready(function() {
-  var fixer = function( data ) {
-      $( ".result" ).html( data );
-  };
-   $('#play_button').click(function() {
-      $.post( "/",'{command_container : {command : "list_songs"}}', listSongs);
-   }); 
+    $.post( "/",'{command_container : {command : "list_songs"}}', listSongs);
 });
 
